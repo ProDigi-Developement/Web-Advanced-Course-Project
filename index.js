86fa1b2d1069b7b5b10c708477ba93d8d9def045
Denis Gois <denisdwtg@gmail.com>
2018-03-12 20:48:22 -0400

    Add Job List component and route

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-let jobComponent = Vue.component('lc-job', {
-    // options
-    props: ['joby'],
-
-    template: `
-  	<h3>jo</h3>
-  `,
-    data: function() {
-        return {
-
-        };
-    }
-})
-
-let ApplicantListComponent = Vue.component('lc-applicant-list', {
-    template: `
-  <div>
-    <h1>Applicant List</h1>
-    <ul>
-      <li v-for="app in applicants">
-        {{app}}
-      </li>
-    </ul>
-  </div>
-`,
-    data: function() {
-        return {
-            applicants: [
-                "Jen",
-                "Mayank",
-                "Mayur",
-                "Arsh",
-                "Vishnu",
-                "Kevin",
-                "Denis"
-            ],
-        };
-    },
-    methods: {
-
-    },
-});
-
 function Job(parameters) {
     this.title = parameters.title;
     this.startDate = parameters.startDate;
@@ -95,9 +52,75 @@ let jobs = [
     })
 ];
 
+let JobListComponent = Vue.component('job-list', {
+    template: `
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" v-for="job in jobs">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{job.title}}</h5>
+            <small>{{job.postDate}}</small>
+          </div>
+          <p class="mb-1">{{job.description}}</p>
+          <div class="d-flex justify-content-between">
+            <small>Salary {{job.salary}}</small>
+            <span class="badge badge-primary badge-pill">14</span>
+          </div>
+        </a>
+      </div>
+    `,
+    data: function() {
+        return {
+            jobs: jobs
+        };
+    }
+});
+
+let jobComponent = Vue.component('lc-job', {
+    // options
+    props: ['joby'],
+
+    template: `
+  	<h3>jo</h3>
+  `,
+    data: function() {
+        return {
+
+        };
+    }
+})
+
+let ApplicantListComponent = Vue.component('lc-applicant-list', {
+    template: `
+  <div>
+    <h1>Applicant List</h1>
+    <ul>
+      <li v-for="app in applicants">
+        {{app}}
+      </li>
+    </ul>
+  </div>
+`,
+    data: function() {
+        return {
+            applicants: [
+                "Jen",
+                "Mayank",
+                "Mayur",
+                "Arsh",
+                "Vishnu",
+                "Kevin",
+                "Denis"
+            ],
+        };
+    },
+    methods: {
+
+    },
+});
+
 const routes = [
-    { path: '/applicants', component: ApplicantListComponent },
-    { path: '/jobs', component: jobComponent },
+    { path: '/jobs', component: JobListComponent },
+    { path: '/applicants', component: ApplicantListComponent }
 ]
 
 const router = new VueRouter({
