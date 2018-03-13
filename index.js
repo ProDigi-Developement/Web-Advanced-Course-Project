b29c738db2550c7f0ff847e7052cde8c8229f118
Denis Gois <denisdwtg@gmail.com>
2018-03-12 20:23:09 -0400

    Configure vuejs router

--- index.js
+++ index.js
@@ -1,121 +1,131 @@
-let ApplicantListComponent = Vue.component('lc-applicant-list', {
-  template: `
-    <div>
-    	<h1>Applicant List</h1>
-      <ul>
-        <li v-for="app in applicants">
-          {{app}}
-        </li>
-      </ul>
-    </div>
-  `,
-  data: function() {
-  	return {
-      applicants: [
-        "Jen",
-        "Mayank",
-        "Mayur",
-        "Arsh",
-        "Vishnu",
-        "Kevin",
-        "Denis"
-      ],
-    };
-  },
-  methods: {
-
-  },
-});
-
-
 let jobComponent = Vue.component('lc-job', {
-  // options
-  props: ['joby'],
+    // options
+    props: ['joby'],
 
-  template: `
+    template: `
   	<h3>jo</h3>
   `,
-  data: function() {
-  	return {
+    data: function() {
+        return {
 
-    };
-  }
+        };
+    }
 })
 
+let ApplicantListComponent = Vue.component('lc-applicant-list', {
+    template: `
+  <div>
+    <h1>Applicant List</h1>
+    <ul>
+      <li v-for="app in applicants">
+        {{app}}
+      </li>
+    </ul>
+  </div>
+`,
+    data: function() {
+        return {
+            applicants: [
+                "Jen",
+                "Mayank",
+                "Mayur",
+                "Arsh",
+                "Vishnu",
+                "Kevin",
+                "Denis"
+            ],
+        };
+    },
+    methods: {
+
+    },
+});
+
 function Job(parameters) {
-	this.title = parameters.title;
-  this.startDate = parameters.startDate;
-  this.description = parameters.description;
-  this.stack = parameters.stack;
-  this.numberApplied = parameters.numberApplied;
-  this.salary = parameters.salary;
-  this.postDate = parameters.postDate;
-  this.applicantList = parameters.applicantList;
+    this.title = parameters.title;
+    this.startDate = parameters.startDate;
+    this.description = parameters.description;
+    this.stack = parameters.stack;
+    this.numberApplied = parameters.numberApplied;
+    this.salary = parameters.salary;
+    this.postDate = parameters.postDate;
+    this.applicantList = parameters.applicantList;
 }
 
 
 let jobs = [
-	new Job({
-  	title : 'frontend developer 1',
-    startDate : new Date(),
-    description : 'Looking for a web developer who had 15 years of Vue.js',
-    stack: {
-    	backend: ['Node', 'Express', 'SQL'],
-      frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
-    },
-  	numberApplied: 0,
-    salary: 200000,
-    postDate: new Date(),
-    applicantList: [],
-  }),
-  	new Job({
-  	title : 'frontend developer 1',
-    startDate : new Date(),
-    description : 'Looking for a web developer who had 15 years of Vue.js',
-    stack: {
-    	backend: ['Node', 'Express', 'SQL'],
-      frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
-    },
-  	numberApplied: 0,
-    salary: 200000,
-    postDate: new Date(),
-    applicantList: [],
-  }),
-  	new Job({
-  	title : 'frontend developer 1',
-    startDate : new Date(),
-    description : 'Looking for a web developer who had 15 years of Vue.js',
-    stack: {
-    	backend: ['Node', 'Express', 'SQL'],
-      frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
-    },
-  	numberApplied: 0,
-    salary: 200000,
-    postDate: new Date(),
-    applicantList: [],
-  })
+    new Job({
+        title: 'frontend developer 1',
+        startDate: new Date(),
+        description: 'Looking for a web developer who had 15 years of Vue.js',
+        stack: {
+            backend: ['Node', 'Express', 'SQL'],
+            frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
+        },
+        numberApplied: 0,
+        salary: 200000,
+        postDate: new Date(),
+        applicantList: [],
+    }),
+    new Job({
+        title: 'frontend developer 1',
+        startDate: new Date(),
+        description: 'Looking for a web developer who had 15 years of Vue.js',
+        stack: {
+            backend: ['Node', 'Express', 'SQL'],
+            frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
+        },
+        numberApplied: 0,
+        salary: 200000,
+        postDate: new Date(),
+        applicantList: [],
+    }),
+    new Job({
+        title: 'frontend developer 1',
+        startDate: new Date(),
+        description: 'Looking for a web developer who had 15 years of Vue.js',
+        stack: {
+            backend: ['Node', 'Express', 'SQL'],
+            frontend: ['Vue', 'HTML', 'Sass', 'Bootstrap', 'Javascript'],
+        },
+        numberApplied: 0,
+        salary: 200000,
+        postDate: new Date(),
+        applicantList: [],
+    })
 ];
 
-new Vue({
-  el: '#app',
-  components: {
-  	'lc-job': jobComponent,
-    'lc-applicant-list': ApplicantListComponent,
-  },
-  data: {
-    message: 'Hello Vue.js!',
-    currentPage: 'job-list',
-    currentJob: null,
-    jobList:jobs,
-  },
-  methods: {
-  	setPage: function(page) {
-    	this.currentPage = page;
-    },
-    selectJob: function(job) {
-    	this.currentJob = job;
-      this.setPage('job-details');
-    }
-  },
+const routes = [
+    { path: '/applicants', component: ApplicantListComponent },
+    { path: '/jobs', component: jobComponent },
+]
 
+const router = new VueRouter({
+    routes
 })
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        'lc-job': jobComponent,
+        'lc-applicant-list': ApplicantListComponent,
+    },
+    data: {
+        message: 'Hello Vue.js!',
+        currentPage: 'job-list',
+        currentJob: null,
+        jobList: jobs,
+    },
+    methods: {
+        setPage: function(page) {
+            console.log(page);
+
+            this.currentPage = page;
+        },
+        selectJob: function(job) {
+            this.currentJob = job;
+            this.setPage('job-details');
+        }
+    },
+    router
+}).$mount('#app');
