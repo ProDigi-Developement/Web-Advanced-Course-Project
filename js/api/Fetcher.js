bd7f7f43ddecd4ad8ebef6a4f86a61f181182289
Caio Dias <kiobreno@gmail.com>
2018-07-31 19:59:05 -0400

    Add login method

--- js/api/Fetcher.js
+++ js/api/Fetcher.js
@@ -11,6 +11,7 @@ class Fetcher {
 
         this.token =
       'eyJhbGciOiJSUzI1NiIsImtpZCI6IjM2OTExYjU1NjU5YTI5YmU3NTYyMDYzYmIxNzc2NWI1NDk4ZTgwZDYifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvLWRpZ2ktYWR2YW5jZWQiLCJhdWQiOiJwcm8tZGlnaS1hZHZhbmNlZCIsImF1dGhfdGltZSI6MTUzMzA2NzQ0NCwidXNlcl9pZCI6IlBsaEd5Y0VnUzdZS1BYeHF5aE9NME9IVTR4MzIiLCJzdWIiOiJQbGhHeWNFZ1M3WUtQWHhxeWhPTTBPSFU0eDMyIiwiaWF0IjoxNTMzMDY3NDQ1LCJleHAiOjE1MzMwNzEwNDUsImVtYWlsIjoiY2Fpb0B0cmFpbmVycGwudXMiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiY2Fpb0B0cmFpbmVycGwudXMiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.g6kG1WkTsB8TPGrMIkZTuPNmHmNm4Y94qWBVLW7oOcE7SIDGXQCg566s3A7tBQChyChwQ_4N7CnlyA3TN7656UBx8LeUxPjkEq6bECrMYh1_BFRUm5WkETbL6NkNIkSZQ4LLcuweZTtateecTg-eLQM4dcS2SKEu4f5y74dmn6PoMlUIMnRC5V-sdXNx58T6GZYfSxaz0fKn4BIzljh3ejDLe6NO7bdFjpcN6p440rW-5SWsVMPJwDCelcnUOEr4-4uLN06hfS91SFjZ_Gwn05pohpj53NY77En4qzKEczg5P9SIo5qUaBchAOC5qVVHdmA6bZC3Gsle3V8G9J1zUQ';
+        this.loginUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB8m7LmEtH4wNjFKEfKnaUcUUJVdp1Ntx4';
 
         return Fetcher.instance;
     }
@@ -24,6 +25,23 @@ class Fetcher {
             const data = await fetching.json();
 
             return data;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    async login() {
+        try {
+            let body = {
+                "email": "caio@trainerpl.us",
+                "password": "121088",
+                "returnSecureToken": true
+            };
+
+            const fetching = await fetch(`${this.loginUrl}`, {'method': "POST", 'body': body, 'headers': {'Content-Type': 'application/json'} });
+            const data = await fetching.json();
+
+            return data.idToken;
         } catch (err) {
             throw new Error(err);
         }
