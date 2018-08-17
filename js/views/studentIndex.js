bc06d3fc8e0907617c4e71d43f81eb445e26331f
Bruno Santos <ebrunosan@gmail.com>
2018-08-17 19:49:19 -0400

    sorting combo box filter term

--- js/views/studentIndex.js
+++ js/views/studentIndex.js
@@ -25,50 +25,6 @@ class StudentView {
             });
     }
 
-    loadHtmlFilterCmb() {
-        // Populating Term combo
-        const cmbTermEl = document.getElementById( 'cmbTermFilter' );
-        cmbTermEl.options[ cmbTermEl.options.length ] = new Option( 'All' );
-
-        // Creating new array with unique 'term' & mapping to the combo
-        [...new Set( this.studentsArr.map( st => st.props.term ) ) ]
-            .map( term => {
-                return cmbTermEl.options[ cmbTermEl.options.length ] = new Option( term );
-            });
-    } 
-
-    static uniqBy(a, key) {
-        var seen = {};
-        return a.filter( function(item) {
-            var k = key( item );
-            return seen.hasOwnProperty(k) ? false : ( seen[k] = true );
-        })
-    }
-
-    loadHtmlTable ( studentsArr ) {
-        const tableBody = studentsArr.map( st => { 
-                return `<tr> 
-                    <td><img src="${st.props.photo}"></img></td> 
-                    <td>${st.props.name}</td> 
-                    <td>${st.props.email}</td> 
-                    <td>${st.props.term}</td> 
-                    <td>${st.props.goal}</td> 
-                    <td>${st.props.githubLink}</td> 
-                </tr>`;
-            })
-            .join('');
-        document.querySelector( '#students-data' ).innerHTML = 
-            `<thead>
-                <th>Photo</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Term</th>
-                <th>Goal</th>
-                <th>Github link</th>
-            </thead>
-            <tbody> ${tableBody} </tbody>`;
-    }
-
     applyFilter () {
         const cmbTermEl = document.getElementById( 'cmbTermFilter' );
         let filteredArr = this.studentsArr;
@@ -81,6 +37,47 @@ class StudentView {
         }
 
         this.loadHtmlTable ( filteredArr );
+    }
+
+    loadHtmlFilterCmb() {
+        // Populating Term combo
+        const cmbTermEl = document.getElementById( 'cmbTermFilter' );
+        cmbTermEl.options[ cmbTermEl.options.length ] = new Option( 'All' );
+
+        // Creating a sorted array with unique 'term' & mapping into the combo element
+        [...new Set( this.studentsArr.map( st => st.props.term ).sort() ) ]
+            .map( term => {
+                return cmbTermEl.options[ cmbTermEl.options.length ] = new Option( term );
+            });
+    } 
+
+    loadHtmlTable ( studentsArr ) {
+        const tableBody = studentsArr.map( st => { return `<tr> 
+                    <td><img src="${st.props.photo}"></img></td> 
+                    <td>${st.props.name}</td> 
+                    <td>${st.props.email}</td> 
+                    <td>${st.props.term}</td> 
+                    <td>${st.props.goal}</td> 
+                    <td>${st.props.githubLink}</td> </tr>`;
+            }).join('');
+        document.querySelector( '#students-data' ).innerHTML = 
+            `<thead>
+                <th>Photo</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Term</th>
+                <th>Goal</th>
+                <th>Github link</th>
+            </thead>
+            <tbody> ${tableBody} </tbody>`;
+    }
+
+    static uniqBy(a, key) {
+        var seen = {};
+        return a.filter( function(item) {
+            var k = key( item );
+            return seen.hasOwnProperty(k) ? false : ( seen[k] = true );
+        })
     }
 
 }
